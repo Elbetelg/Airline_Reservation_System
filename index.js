4736439484e55447624ff5e056eccdc54fe0016d
Elbetelg <elbetelgetu2020@gmail.com>
2026-06-12 02:18:20 +0100

    connect MongoDB Atlas to Express app

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-// Import the Express framework
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Import the Express and Mongoose framework
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Create an instance of an Express application
 const app = express();
 
 // Middleware: allows the server to understand JSON in request bodies
 app.use(express.json());
+
+// Connect to MongoDB Atlas using the connection string from .env
+mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
+    family: 4
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('MongoDB connection error:', err));
 
 // Basic test route - when someone visits the root URL, send a message back
 app.get('/', (req, res) => {
